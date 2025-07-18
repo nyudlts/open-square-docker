3e3b6e15d59ab28600b78783eaa1916eef2a40a4
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-07-17 17:56:17 -0700

    Make two solr cores

--- src/solr/docker-entrypoint-initdb.d/init.sh
+++ src/solr/docker-entrypoint-initdb.d/init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
+# v0 core
 /opt/docker-solr/scripts/precreate-core open-square-metadata /conf/open-square-metadata
+# v1 core
+/opt/docker-solr/scripts/precreate-core open-square-metadata-v1 /conf/open-square-metadata-v1
+
+echo "started v0 and v1 cores"
